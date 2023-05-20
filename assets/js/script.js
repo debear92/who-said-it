405bba4d9ae02edf9d330ce8de8daaf44317e1d0
Deborah Galasso <galassodeborah@outlook.com>
2023-05-20 16:36:37 +0000

    Fix styling of the quiz page, setup scheleton of javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+let quizQuestions = [
+    {
+        question: '"Your faithless love is the only hoax I believe in"', 
+        answer: 'Taylor Swift',
+        explanation: '"hoax" from the album "folklore"'
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    },
+
+    {
+        question:
+        answer:
+        explanation:
+    }
+]
