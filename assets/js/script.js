ef8d5268430865762ad266a4ddb2048136d9c0ad
Deborah Galasso <galassodeborah@outlook.com>
2023-05-21 09:22:25 +0000

    Add function to randomize questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,79 @@
-const quizQuestion = [
+//array containing quiz questions
+const quotes = [
     {
-        question: '"Your faithless love\'s the only hoax I believe in"', 
-        answer: 'Taylor Swift',
+        quote: '"Your faithless love\'s the only hoax I believe in"', 
+        author: 'Taylor Swift',
         explanation: '"hoax" from the album "folklore"'
     },
 
     {
-        question: '“Hell is empty and all the devils are here.”',
-        answer: 'Shakespeare',
+        quote: '“Hell is empty and all the devils are here.”',
+        author: 'Shakespeare',
         explanation: 'From the play "The Tempest"'
     },
 
     {
-        question: '"Do you miss the rogue who coaxed you into paradise and left you there?"',
-        answer: 'Taylor Swift',
+        quote: '"Do you miss the rogue who coaxed you into paradise and left you there?"',
+        author: 'Taylor Swift',
         explanation: '"coney island" from the album "evermore"'
     },
 
     {
-        question: '"Did the twin flame bruise pain you blue?"',
-        answer: 'Taylor Swift',
+        quote: '"Did the twin flame bruise pain you blue?"',
+        author: 'Taylor Swift',
         explanation: '"All Too Well (10 Minute Version) (Taylor\'s Version) (From The Vault)." from the album "Red (Taylor\'s Version)"'
     },
 
     {
-        question: '"I\'d meet you where the spirit meets the bones in a faith-forgotten land"',
-        answer: 'Taylor Swift',
+        quote: '"I\'d meet you where the spirit meets the bones in a faith-forgotten land"',
+        author: 'Taylor Swift',
         explanation: '"ivy" from the album "evermore"'
     },
 
     {
-        question: '"How can it be said I am alone when all the world is here to look on me?"',
-        answer: 'Shakespeare',
+        quote: '"How can it be said I am alone when all the world is here to look on me?"',
+        author: 'Shakespeare',
         explanation: 'From the play "A midsummer night\'s dream"'
     },
 
     {
-        question: '"Can\'t not think of all the cost and the things that will be lost"',
-        answer: 'Taylor Swift',
+        quote: '"Can\'t not think of all the cost and the things that will be lost"',
+        author: 'Taylor Swift',
         explanation: '"evermore" from the album evermore'
     },
 
     {
-        question: '"And I\'ll stay... forgetting any other home but this."',
-        answer: 'Shakespeare',
+        quote: '"And I\'ll stay... forgetting any other home but this."',
+        author: 'Shakespeare',
         explanation: 'From the play "Romeo and Juliet"'
     },
 
     {
-        question: '"When I was at home, I was in a better place."',
-        answer: 'Shakespeare',
+        quote: '"When I was at home, I was in a better place."',
+        author: 'Shakespeare',
         explanation: 'From the play "As you like it"'
     },
 
     {
-        question: '"Is it romantic how all my elegies eulogize me?"',
-        answer: 'Taylor Swift',
+        quote: '"Is it romantic how all my elegies eulogize me?"',
+        author: 'Taylor Swift',
         explanation: '"The lakes" from the album "folklore (deluxe version)'
     }
 ]
 
-let questionContainer = document.getElementById("question");
+//variables
+const quizContainer = document.getElementById("quiz-container");
+const questionContainer = document.getElementById('question-container');
+const option = document.getElementsByClassName("option");
 let currentQuestion = 0;
 let score = 0;
 
-document.addEventListener("DOMContentLoaded", getQuestion ())
+//event listeners
+document.addEventListener("DOMContentLoaded", getQuestion());
 
 function getQuestion() {
-    let currentQuizQuestion = quizQuestion[currentQuestion];
-    questionContainer.innerText = currentQuizQuestion.question;
-}
-
-
-function checkAnswer() {
-    console.log("button click")
-    let currentQuizQuestion = quizQuestion[currentQuestion];
-
+    const randomQuote = Math.floor(Math.random() *quotes.length);
+    const currentQuizQuestion = quotes[randomQuote];
+    const author = quotes[randomQuote].author;
+    questionContainer.innerText = currentQuizQuestion.quote;
 }
