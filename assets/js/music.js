9ad194afdcff9cac641742269598b7310192bda5
Deborah Galasso <galassodeborah@outlook.com>
2023-05-22 17:02:12 +0000

    Change plan for the music part of the project, instead of having multiple background songs I am having sounds for various events

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,22 +1,5 @@
-let music = [
-    "/assets/music/champagne-problems.mp3",
-    "/assets//music/cruel-summer.mp3",
-    "/assets//music/dont-blame-me.mp3",
-    "/assets//music/get-away-car.mp3",
-    "/assets//music/miss-americana-and-the-hearbreak-prince.mp3",
-    "/assets//music/speak-now.mp3",
-    "/assets//music/the-great-war.mp3",
-    "/assets//music/wouldve-couldve-should-ve.mp3"
-];
-
-let audioElement = document.getElementById("random-song");
-let startStop = document.getElementById("audio");
-let start = document.getElementsByClassName("audio-on");
-let stop = document.getElementsByClassName("audio-off");
-startStop.addEventListener('click', playRandomMusic);
-
-function playRandomMusic() {
-    const randomIndex = Math.floor(Math.random() * music.length);
-    audioElement.src = music[randomIndex];
-    audioElement.play();
-}
+const questionRightSound 
+const questionWrongSound
+const audioOn = document.getElementsByClassName('audio-on');
+const audioOff = document.getElementsByClassName('audio-off');
+const audioButton = document.getElementById('audio');
