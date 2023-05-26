690a95990e1a151fc075165221e2b3a4eb171bd4
Deborah Galasso <galassodeborah@outlook.com>
2023-05-26 17:28:52 +0000

    Add function to change audio on and off icon

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,8 +1,24 @@
 let myAudio = document.getElementById("audio");
+let onIcon = document.getElementsByClassName("audio-on")[0];
+let offIcon = document.getElementsByClassName("audio-off")[0];
+onIcon.style.display = "none";
+offIcon.style.display = "in-line";
 let isPlaying = false;
 
 myAudio.addEventListener('click', togglePlay);
 
 function togglePlay() {
-    console.log("play");
+    isPlaying ? audioOff() : audioOn();
+};
+
+function audioOn(){
+   isPlaying = true;
+   onIcon.style.display = "in-line";
+   offIcon.style.display = "none";
+}
+
+function audioOff() {
+    isPlaying = false;
+    onIcon.style.display = "none";
+    offIcon.style.display = "in-line";
 }
