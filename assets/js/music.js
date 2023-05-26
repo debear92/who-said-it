2a6c889f527c14cf5ba7201c31a7dd690d55e4e5
Deborah Galasso <galassodeborah@outlook.com>
2023-05-26 18:07:58 +0000

    Add conditions for the audio to be always be off when first loading the page, set the starting volume of the sounds

--- assets/js/music.js
+++ assets/js/music.js
@@ -3,18 +3,29 @@ const audioOff = document.getElementById("off");
 const correctAudio = new Audio('assets/sound/clapping.mp3');
 const wrongAudio = new Audio ('assets/sound/trouble.mp3');
 
+//Adding conditions for the audio to always be off when first loading the page
+audioOn.style.display = "none";
+audioOff.style.display = "block";
+correctAudio.muted = true;
+wrongAudio.muted = true;
+
+//set initial volume level
+correctAudio.volume = 0.5;
+wrongAudio.volume = 0.5;
+
+//event listeners
 audioOn.addEventListener("click", toggleAudio);
 audioOff.addEventListener("click", toggleAudio);
 
 function toggleAudio() {
     if (audioOn.style.display === "none") {
-        audioOn.style.display = "in-line";
+        audioOn.style.display = "block";
         audioOff.style.display = "none";
         correctAudio.muted = false;
         wrongAudio.muted = false;
     } else {
         audioOn.style.display = "none";
-        audioOff.style.display = "in-line";
+        audioOff.style.display = "block";
         correctAudio.muted = true;
         wrongAudio.muted = true;
     }
