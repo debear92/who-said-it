d10cf58ea40af14c7ae98d0b4733ac969ecfedec
Deborah Galasso <galassodeborah@outlook.com>
2023-05-26 17:15:42 +0000

    Previous version was not working as expected, set up new toggle function

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,1 +1,8 @@
-//constants
+let myAudio = document.getElementById("audio");
+let isPlaying = false;
+
+myAudio.addEventListener('click', togglePlay);
+
+function togglePlay() {
+    console.log("play");
+}
