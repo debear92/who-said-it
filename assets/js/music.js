06e9143e6b9801ea5e19b1a3f39780dff8a031af
Deborah Galasso <galassodeborah@outlook.com>
2023-05-26 16:19:59 +0000

    Add sound for wrong answer

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,12 +1,12 @@
 const questionRightSound = new Audio("assets/sound/clapping.mp3");
-const questionWrongSound
+const questionWrongSound = new Audio("assets/sound/trouble.mp3");
 const audioOn = document.getElementsByClassName('audio-on');
 const audioOff = document.getElementsByClassName('audio-off');
 const audioButton = document.getElementById('audio');
 
 
-function toggleAudio 
-function turnOn
-function turnOff 
-function questionRight
-function questionWrong
+function toggleAudio()
+function turnOn()
+function turnOff()
+function questionRight()
+function questionWrong()
